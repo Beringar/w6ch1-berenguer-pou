7440ed8971a82569dc533f02bd3339563bbd5070
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-16 01:59:54 +0100

    added button component

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,7 +1,13 @@
+import Button from "../Button/Button";
 import "./todo.css";
 const Todo = ({ todo }) => (
   <article className={`todo ${todo.notdone ? "todo--notdone" : ""}`}>
     <p>{todo.text}</p>
+    <Button
+      text={todo.notdone ? "Set as not resolved" : "Mark as solved"}
+      className={todo.notdone ? "button--notdone" : "button-done"}
+      actionOnClick={() => null}
+    />
   </article>
 );
 
