45158a855f0fa8d0469efbd4ced60da725e4f7e5
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-16 03:13:41 +0100

    added fortawesome package / added buttons mark resolved/set unresolved

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,14 +1,35 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../Button/Button";
 import "./todo.css";
-const Todo = ({ todo }) => (
-  <article className={`todo ${todo.notdone ? "todo--notdone" : ""}`}>
-    <p>{todo.text}</p>
-    <Button
-      text={todo.notdone ? "Set as not resolved" : "Mark as solved"}
-      className={todo.notdone ? "button--notdone" : "button-done"}
-      actionOnClick={() => null}
-    />
-  </article>
-);
+import { deleteTodoThunk } from "../../redux/thunks/todosThunks";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const TodoWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 12px;
+  padding: 8px 30px;
+`;
+
+const Todo = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const deleteTodo = () => dispatch(deleteTodoThunk(todo.id));
+
+  return (
+    <TodoWrapper className={`todo ${todo.notdone ? "todo--notdone" : ""}`}>
+      <p>{todo.text}</p>
+      <Button
+        text={todo.notdone ? "Set as not resolved" : "Mark as solved"}
+        className={todo.notdone ? "button--notdone" : "button-done"}
+        actionOnClick={() => null}
+      />
+      <FontAwesomeIcon icon={faTrashCan} onClick={deleteTodo} />
+    </TodoWrapper>
+  );
+};
 
 export default Todo;
--- src/redux/thunks/todosThunks.js
+++ src/redux/thunks/todosThunks.js
@@ -1,7 +1,20 @@
-import { loadTodosAction } from "../actions/actionsCreators";
+import { loadTodosAction, deleteTodoAction } from "../actions/actionsCreators";
 
 export const loadTodosThunk = async (dispatch) => {
   const response = await fetch(process.env.REACT_APP_HEROKKU_API_URL);
   const todos = await response.json();
   dispatch(loadTodosAction(todos));
+};
+
+export const deleteTodoThunk = (id) => async (dispatch) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_HEROKKU_API_URL}${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  if (response.ok) {
+    dispatch(deleteTodoAction(id));
+  }
 };
